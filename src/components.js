ff1069f3f765df859acb956075da3ef4f5f5476a
Rojin Koç <rojinin.koc@gmail.com>
2025-03-04 12:30:05 +0300

    hover

--- src/components.js
+++ src/components.js
@@ -8,5 +8,8 @@ export const Title = styled.h1`
     ${props => props.theme === 'dark' && css`
         background: #000;
         color: #fff;
+        &:hover {
+            background: blue;
+        }
     `}
 `;
