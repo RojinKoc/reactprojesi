ed5b503ceb81c75c1526a5b3b8f34ee804198949
Rojin Koç <rojinin.koc@gmail.com>
2025-03-04 11:55:07 +0300

    .env

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h3> { process.env.NODE_ENV} </h3>
+      <p> {process.env.REACT_APP_API_URL} </p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
