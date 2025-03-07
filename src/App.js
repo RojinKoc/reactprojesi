f8c3c5596a80e0a34e29283daff1a537f614d160
Rojin Koç <rojinin.koc@gmail.com>
2025-03-07 09:05:43 +0300

    jsx

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import './App.css';
 
 function App() {
   let a = 15;
+  const firstName = "Rojin";
+  let sonuc = true;
+
   return (
     <main>
       <h1>DENEME</h1>
       <p>a değişkeninin değeri: {a} </p>
+      <p> Müşterinin adı : {firstName} </p>
+      {sonuc ? <p> Doğru!</p> : <p>Yanlış!</p>}
     </main>
   );
 }
