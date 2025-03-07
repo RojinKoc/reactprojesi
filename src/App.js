a71f118f995203636b1c57a612bda2d31c1c6338
Rojin Koç <rojinin.koc@gmail.com>
2025-03-07 09:34:47 +0300

    jsx

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   let a = 15;
   const firstName = "Rojin";
   let sonuc = true;
+  let isimler =[
+    "Ali",
+    "Ayşe",
+    "Fatma",
+    "Veli"
+  ]
 
   return (
     <main>
@@ -11,6 +17,19 @@ function App() {
       <p>a değişkeninin değeri: {a} </p>
       <p> Müşterinin adı : {firstName} </p>
       {sonuc ? <p> Doğru!</p> : <p>Yanlış!</p>}
+      
+      {
+        isimler.map((isim,index) =>(
+          <div style={{
+            backgroundColor: 'orange',
+            border: '1px solid black'
+          }} key={index}> 
+            {isim}
+          </div>
+        ))
+      }
+
+
     </main>
   );
 }
